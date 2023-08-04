1a8dbaccde2d290b3eb0e03b8034c9c31b24a0ce
idelvandro <idelvandrof@.gmail.com>
2023-08-04 13:33:04 -0400

    teste de falha

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Idelvandro Fonseca.")
+        self.assertRegex(result.data.decode(), "Pipeline não DevOps - Idelvandro Fonseca.")
 
 
 if __name__ == "__main__":
